77507a2cf154bf8d8af01bc94cbc080f1f1ccc3c
Andrei Gramakov <mail@agramakov.me>
2025-05-31 13:11:57 +0000

    More feature enable for the core test

--- tests/unit/test_core.cpp
+++ tests/unit/test_core.cpp
@@ -5,16 +5,25 @@
 #include "ut_callback.h"
 
 struct CoreTestFixture {
+public:
+    static bool callback_is_set;
+    
     CoreTestFixture() {
         // Per-test setup
+        if (!callback_is_set) {
+            ulog_add_callback(ut_callback, nullptr, LOG_TRACE);
+            callback_is_set = true;
+        }
         ulog_set_level(LOG_TRACE);
         ulog_set_quiet(false);
-        ulog_add_callback(ut_callback, nullptr, LOG_TRACE);
         ut_callback_reset();
     }
 
     ~CoreTestFixture() = default;
 };
+
+bool CoreTestFixture::callback_is_set = false;
+
 
 TEST_CASE_FIXTURE(CoreTestFixture, "Base") {
     log_trace("This is a TRACE message: %d", 123);
