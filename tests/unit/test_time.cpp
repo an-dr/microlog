daa39156a8058cc8ff08eba9e2194c78edfc7db8
Aidan Costello <aidcostello@gmail.com>
2025-06-17 22:43:13 +0000

    add whitespace check for prefix, clang-format

--- tests/unit/test_time.cpp
+++ tests/unit/test_time.cpp
@@ -11,14 +11,13 @@ static void test_prefix(ulog_Event *ev, char *prefix, size_t prefix_size) {
 }
 
 struct TimeTestFixture {
-public:
+  public:
     static bool callback_is_set;
 
     TimeTestFixture() {
         // Per-test setup
         if (!callback_is_set) {
             ulog_add_callback(ut_callback, nullptr, LOG_TRACE);
-            callback_is_set = true;
         }
         ulog_set_quiet(false);
         ut_callback_reset();
@@ -36,7 +35,7 @@ static void _get_time_bounds(time_t &before, time_t &after) {
     REQUIRE(after != (time_t)(-1));
 }
 
-static void _check_time_fields(int hh, int mm, int ss, const time_t &before, 
+static void _check_time_fields(int hh, int mm, int ss, const time_t &before,
                                const time_t &after) {
     auto before_tm = gmtime(&before);
     CHECK_GE(hh, before_tm->tm_hour);
@@ -49,8 +48,9 @@ static void _check_time_fields(int hh, int mm, int ss, const time_t &before,
     CHECK_LE(ss, after_tm->tm_sec);
 }
 
-static void _check_date_time_fields(int yr, int mo, int d, int hh, int mm, int ss, 
-                                    const time_t &before, const time_t &after) {
+static void _check_date_time_fields(int yr, int mo, int d, int hh, int mm,
+                                    int ss, const time_t &before,
+                                    const time_t &after) {
     auto before_tm = gmtime(&before);
     CHECK_GE(yr, before_tm->tm_year + 1900);
     CHECK_GE(mo, before_tm->tm_mon + 1);
@@ -72,7 +72,16 @@ void _check_console_time() {
 
     // Parse time values, ensure 3 matches
     int hh, mm, ss;
-    REQUIRE(sscanf(ut_callback_get_last_message(), "%d:%d:%d ", &hh, &mm, &ss) == 3);
+    REQUIRE(sscanf(ut_callback_get_last_message(), "%d:%d:%d ", &hh, &mm,
+                   &ss) == 3);
+
+#ifdef FEATURE_CUSTOM_PREFIX
+    // Should be no space after time
+    REQUIRE(ut_callback_get_last_message()[9] != ' ');
+#else   // FEATURE_CUSTOM_PREFIX
+    // If no custom prefix, there should be a space after the time
+    REQUIRE(ut_callback_get_last_message()[9] == ' ');
+#endif  // FEATURE_CUSTOM_PREFIX
 
     _check_time_fields(hh, mm, ss, before, after);
 }
@@ -99,7 +108,8 @@ void _check_file_time() {
 
     // Parse time values, ensure 6 matches
     int yr, mo, d, hh, mm, ss;
-    REQUIRE(sscanf(buffer, "%d-%d-%d %d:%d:%d ", &yr, &mo, &d, &hh, &mm, &ss) == 6);
+    REQUIRE(sscanf(buffer, "%d-%d-%d %d:%d:%d ", &yr, &mo, &d, &hh, &mm, &ss) ==
+            6);
 
     _check_date_time_fields(yr, mo, d, hh, mm, ss, before, after);
 }
@@ -108,11 +118,11 @@ TEST_CASE_FIXTURE(TimeTestFixture, "Check time without prefix") {
     _check_console_time();
     _check_file_time();
 }
-
+#ifdef FEATURE_CUSTOM_PREFIX
 TEST_CASE_FIXTURE(TimeTestFixture, "Check time with prefix") {
     ulog_set_prefix_fn(test_prefix);
 
     _check_console_time();
     _check_file_time();
 }
-
+#endif  // FEATURE_CUSTOM_PREFIX
