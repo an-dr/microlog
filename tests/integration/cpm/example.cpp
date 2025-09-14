169e12ec2b46dd0316b1d0ef31f37dd8e6f2417a
an-dr <mail@agramakov.me>
2025-09-14 11:58:20 +0200

    Add cpm tests

---
+++ tests/integration/cpm/example.cpp
@@ -0,0 +1,6 @@
+#include "ulog.h"
+
+int main() {
+    ulog_info("Test message from test package");
+    return 0;
+}
---
+++ tests/integration/cpm/test_cpm.ps1
@@ -0,0 +1,39 @@
+# *************************************************************************
+#
+# Copyright (c) 2025 Andrei Gramakov. All rights reserved.
+#
+# This file is licensed under the terms of the MIT license.  
+# For a copy, see: https://opensource.org/licenses/MIT
+#
+# site:    https://agramakov.me
+# e-mail:  mail@agramakov.me
+#
+# *************************************************************************
+
+$ErrorActionPreference = "Stop"
+Push-Location $PSScriptRoot
+
+try {
+    
+    # Build the test package
+    cmake -B./build/cmake-cpm
+    # $env:microlog_DIR = "$PSScriptRoot/build/cmake/deps/microlog" # - another option to specify the path
+    cmake --build ./build/cmake-cpm --verbose
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Tests failed with exit code $LASTEXITCODE"
+        exit $LASTEXITCODE
+    }
+    
+    Pop-Location
+    
+} catch {
+    
+    Write-Host "An error occurred: $_"
+    Pop-Location
+    exit 1  # Exit the script with a non-zero code to indicate failure
+    
+}
+
+Write-Host "`n[OK] Test completed successfully."
+Pop-Location
+
