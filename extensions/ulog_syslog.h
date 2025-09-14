ec608c2e97dd179261deb612b7e2d64a41103776
Andrei Gramakov <mail@agramakov.me>
2025-09-14 03:25:01 +0200

    Use CMake and meson config features to replace version in the installed files (#127)

--- extensions/ulog_syslog.h
+++ extensions/ulog_syslog.h
@@ -1,9 +1,6 @@
 // *************************************************************************
 //
-// ulog v@{ULOG_VERSION}@ - A simple customizable logging library.
-// https://github.com/an-dr/microlog
-//
-// Extension: Syslog Levels.
+// microlog extension: Syslog Levels.
 //
 // Convenience helpers for using RFC 5424 style syslog severities with
 // microlog without modifying the core library headers or sources.
